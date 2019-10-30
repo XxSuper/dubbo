42323e7afb4de53afb7454eef00ecd4f94911247
xuchao <dreammyle@126.com>
2019-10-30 19:38:06 +0800

    dubbo 集群容错 之 LoadBalance 实现

--- dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/loadbalance/ConsistentHashLoadBalance.java
+++ dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/loadbalance/ConsistentHashLoadBalance.java
@@ -53,32 +53,59 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
         return selector.select(invocation);
     }
 
+    /**
+     * ConsistentHashSelector ，是 ConsistentHashLoadBalance 的内部类，一致性哈希选择器，基于 Ketama 算法。
+     * @param <T>
+     */
     private static final class ConsistentHashSelector<T> {
 
+        /**
+         * 虚拟节点与 Invoker 的映射关系
+         */
         private final TreeMap<Long, Invoker<T>> virtualInvokers;
 
+        /**
+         * 每个 Invoker 对应的虚拟节点数
+         */
         private final int replicaNumber;
 
+        /**
+         * 定义哈希值
+         */
         private final int identityHashCode;
 
+        /**
+         * 取值参数位置数组
+         */
         private final int[] argumentIndex;
 
         ConsistentHashSelector(List<Invoker<T>> invokers, String methodName, int identityHashCode) {
+            // 虚拟节点与 Invoker 的映射关系。
             this.virtualInvokers = new TreeMap<Long, Invoker<T>>();
+            // 设置 identityHashCode 定义哈希值。
             this.identityHashCode = identityHashCode;
+            // 初始化 replicaNumber，每个 Invoker 对应的虚拟节点数，默认为 160 。
             URL url = invokers.get(0).getUrl();
             this.replicaNumber = url.getMethodParameter(methodName, "hash.nodes", 160);
+            // 初始化 argumentIndex，选择 Invoker 时，计算 Hash 值的参数位置数组，默认为第一个参数。
             String[] index = Constants.COMMA_SPLIT_PATTERN.split(url.getMethodParameter(methodName, "hash.arguments", "0"));
             argumentIndex = new int[index.length];
             for (int i = 0; i < index.length; i++) {
                 argumentIndex[i] = Integer.parseInt(index[i]);
             }
+            // 初始化 virtualInvokers 映射关系，循环每个 Invoker 对象。
             for (Invoker<T> invoker : invokers) {
                 String address = invoker.getUrl().getAddress();
+                // 每四个虚拟结点为一组
                 for (int i = 0; i < replicaNumber / 4; i++) {
+                    // 这组虚拟结点得到惟一名称。拼接 address + i 作为虚拟节点名的唯一名称。调用 #md5(value) 方法，计算 MD5 。
                     byte[] digest = md5(address + i);
+                    // Md5 是一个16字节长度的数组，将16字节的数组每四个字节一组，分别对应一个虚拟结点，这就是为什么上面把虚拟结点四个划分一组的原因
+                    // 顺序循环每四个字节。
                     for (int h = 0; h < 4; h++) {
+                        // 对于每四个字节，组成一个long值数值，做为这个虚拟节点的在环中的惟一key
                         long m = hash(digest, h);
+                        // 添加 Invoker 到 virtualInvokers 中。
                         virtualInvokers.put(m, invoker);
                     }
                 }
@@ -86,8 +113,12 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
         }
 
         public Invoker<T> select(Invocation invocation) {
+            // 基于方法参数，获得 KEY
             String key = toKey(invocation.getArguments());
+            // 计算 MD5 值
             byte[] digest = md5(key);
+            // 调用 #hash(digest, hash) 方法，计算 KEY 值。
+            // 调用 #selectForKey(hash) 方法，选一个 Invoker 对象。
             return selectForKey(hash(digest, 0));
         }
 
@@ -102,14 +133,18 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
         }
 
         private Invoker<T> selectForKey(long hash) {
+            // 得到大于当前 key 的那个子 Map ，然后从中取出第一个 key ，就是大于且离它最近的那个 key
             Map.Entry<Long, Invoker<T>> entry = virtualInvokers.tailMap(hash, true).firstEntry();
+            // 不存在，则取 virtualInvokers 第一个
             if (entry == null) {
                 entry = virtualInvokers.firstEntry();
             }
+            // 存在，则返回
             return entry.getValue();
         }
 
         private long hash(byte[] digest, int number) {
+            // 型的数字要 & 0xF F再赋值给 int 类型，其本质原因就是想保持二进制补码的一致性。
             return (((long) (digest[3 + number * 4] & 0xFF) << 24)
                     | ((long) (digest[2 + number * 4] & 0xFF) << 16)
                     | ((long) (digest[1 + number * 4] & 0xFF) << 8)
--- dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/loadbalance/LeastActiveLoadBalance.java
+++ dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/loadbalance/LeastActiveLoadBalance.java
@@ -26,6 +26,30 @@ import java.util.Random;
 
 /**
  * LeastActiveLoadBalance
+ * 实现 AbstractLoadBalance 抽象类，最少活跃调用数，相同活跃数的随机，活跃数指调用前后计数差。
+ * 使慢的提供者收到更少请求，因为越慢的提供者的调用前后计数差会越大。
+ * 相比来说，LeastActiveLoadBalance 是 RandomLoadBalance 的加强版，基于最少活跃调用数。
+ *
+ * 最小活跃数算法实现：
+ * 假定有3台dubbo provider:
+ *
+ * 10.0.0.1:20884, weight=2，active=2
+ * 10.0.0.1:20886, weight=3，active=4
+ * 10.0.0.1:20888, weight=4，active=3
+ * active=2最小，且只有一个2，所以选择10.0.0.1:20884
+ *
+ * 假定有3台dubbo provider:
+ *
+ * 10.0.0.1:20884, weight=2，active=2
+ * 10.0.0.1:20886, weight=3，active=2
+ * 10.0.0.1:20888, weight=4，active=3
+ * active=2最小，且有2个，所以从[10.0.0.1:20884,10.0.0.1:20886 ]中选择；
+ * 接下来的算法与随机算法类似：
+ *
+ * 假设offset=1（即random.nextInt(5)=1）
+ * 1-2=-1<0？是，所以选中 10.0.0.1:20884, weight=2
+ * 假设offset=4（即random.nextInt(5)=4）
+ * 4-2=2<0？否，这时候offset=2， 2-3<0？是，所以选中 10.0.0.1:20886, weight=3
  *
  */
 public class LeastActiveLoadBalance extends AbstractLoadBalance {
@@ -36,28 +60,47 @@ public class LeastActiveLoadBalance extends AbstractLoadBalance {
 
     @Override
     protected <T> Invoker<T> doSelect(List<Invoker<T>> invokers, URL url, Invocation invocation) {
+        // invokers 集合总个数
         int length = invokers.size(); // Number of invokers
+        // 最小的活跃数
         int leastActive = -1; // The least active value of all invokers
+        // 相同最小活跃数的个数
         int leastCount = 0; // The number of invokers having the same least active value (leastActive)
+        // 相同最小活跃数的下标
         int[] leastIndexs = new int[length]; // The index of invokers having the same least active value (leastActive)
+        // 多个最小活跃数 invoker 的总权重
         int totalWeight = 0; // The sum of with warmup weights
+        // 第一个权重，用于于计算是否相同
         int firstWeight = 0; // Initial value, used for comparision
+        // 是否所有权重相同
         boolean sameWeight = true; // Every invoker has the same weight value?
         for (int i = 0; i < length; i++) {
             Invoker<T> invoker = invokers.get(i);
+            // 活跃数，每个 Invoker 的活跃数计算，通过 RpcStatus，在 ActiveLimitFilter && ExecuteLimitFilter 中计算。
             int active = RpcStatus.getStatus(invoker.getUrl(), invocation.getMethodName()).getActive(); // Active number
+            // 权重
             int afterWarmup = getWeight(invoker, invocation); // Weight
+            // 发现更小的活跃数，重新开始
             if (leastActive == -1 || active < leastActive) { // Restart, when find a invoker having smaller least active value.
+                // 记录最小活跃数
                 leastActive = active; // Record the current least active value
+                // 重新统计相同最小活跃数的个数
                 leastCount = 1; // Reset leastCount, count again based on current leastCount
+                // 重新记录最小活跃数下标
                 leastIndexs[0] = i; // Reset
+                // 重新累计总权重
                 totalWeight = afterWarmup; // Reset
+                // 记录第一个最小活跃数 invoker 的权重，第一个权重
                 firstWeight = afterWarmup; // Record the weight the first invoker
+                // 还原权重相同标识
                 sameWeight = true; // Reset, every invoker has the same weight value?
             } else if (active == leastActive) { // If current invoker's active value equals with leaseActive, then accumulating.
+                // 累计相同最小的活跃数
                 leastIndexs[leastCount++] = i; // Record index number of this invoker
+                // 累计总权重
                 totalWeight += afterWarmup; // Add this invoker's weight to totalWeight.
                 // If every invoker has the same weight?
+                // 判断所有权重是否一样
                 if (sameWeight && i > 0
                         && afterWarmup != firstWeight) {
                     sameWeight = false;
@@ -66,12 +109,17 @@ public class LeastActiveLoadBalance extends AbstractLoadBalance {
         }
         // assert(leastCount > 0)
         if (leastCount == 1) {
+            // 如果只有一个最小活跃数则直接返回
             // If we got exactly one invoker having the least active value, return this invoker directly.
             return invokers.get(leastIndexs[0]);
         }
+        // ========== 如下部分，和 RandomLoadBalance 类似 ==========
+        // 存在多个最小活跃数的 invoker 并且各自的权重不相等
         if (!sameWeight && totalWeight > 0) {
+            // 如果权重不相同且权重大于0则按总权重数随机
             // If (not every invoker has the same weight & at least one invoker's weight>0), select randomly based on totalWeight.
             int offsetWeight = random.nextInt(totalWeight) + 1;
+            // 并确定随机值落在哪个片断上
             // Return a invoker based on the random value.
             for (int i = 0; i < leastCount; i++) {
                 int leastIndex = leastIndexs[i];
@@ -80,6 +128,7 @@ public class LeastActiveLoadBalance extends AbstractLoadBalance {
                     return invokers.get(leastIndex);
             }
         }
+        // 如果权重相同或权重为0则均等随机
         // If all invokers have the same weight value or totalWeight=0, return evenly.
         return invokers.get(leastIndexs[random.nextInt(leastCount)]);
     }
